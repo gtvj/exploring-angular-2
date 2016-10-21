0ac9b186392b8a375169f32111f7cf74000c204b
Gwyn Jones <gwyn.jones@nationalarchives.gsi.gov.uk>
2016-10-21 18:04:25 +0100

    Tour of heroes: Master/Details
    Includes everything from the Master/Details section of the tutorial.

--- angular-quickstart/app/app.component.ts
+++ angular-quickstart/app/app.component.ts
@@ -31,17 +31,18 @@ const HEROES: Hero[] = [
         <h1>{{title}}</h1>
         <h2>My Heroes</h2>
         <ul class="heroes">
-            <li *ngFor="let hero of heroes">
+            <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
                 <span class="badge">{{hero.id}}</span> {{hero.name}}
             </li>
         </ul>
-        
-        <h2>{{hero.name}} details!</h2>
-        <div><label>id: </label>{{hero.id}}</div>
-        <div><label>name: </label>
-            <input [(ngModel)]="hero.name" placeholder="name">
+        <div *ngIf="selectedHero">
+            <h2>{{selectedHero.name}} details!</h2>
+            <div><label>id: </label>{{selectedHero.id}}</div>
+            <div><label>name: </label>
+                <input [(ngModel)]="selectedHero.name" placeholder="name">
+            </div>
         </div>`,
-    styles: [`    
+    styles: [ `    
       .selected {
         background-color: #CFD8DC !important;
         color: white;
@@ -89,7 +90,7 @@ const HEROES: Hero[] = [
         margin-right: .8em;
         border-radius: 4px 0 0 4px;
       }
-`]
+` ]
 
 })
 
@@ -98,8 +99,9 @@ const HEROES: Hero[] = [
 export class AppComponent {
     title = 'Tour of heroes';
     heroes = HEROES;
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+    selectedHero: Hero;
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
